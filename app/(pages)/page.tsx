dfbafe597e6773263b963fee83711252129890e8
Alex Pagnotta <ap.alex.pagnotta@gmail.com>
2023-08-10 22:44:15 +0200

    feat: add wild logo to title

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,17 +1,21 @@
-// import WildLogoSVG from "~/assets/svg/wild-logo.svg";
+import { cx } from "class-variance-authority";
+
+import WildLogoSVG from "~/assets/svg/wild-logo.svg";
 import { Text } from "~/features/ui/text";
 
 export default function Home() {
   return (
     <>
-      <Text size="title-1" asChild className="whitespace-pre-wrap text-right text-theme-color-text-secondary">
+      <Text size="title-1" asChild className="relative whitespace-pre-wrap text-right text-theme-color-text-secondary">
         <h1>
           Hello, I’m <span className="text-theme-color-text-primary">Alex Pagnotta</span>,{"\n"}a Frontend Developer
           from Italy.{"\n"}
-          {/* <WildLogoSVG className="inline-block w-36 text-theme-color-text-primary" /> TODO: Implement*/}
           <span className="hidden lg:inline">Currently building </span>
           <span className="lg:hidden">Building </span>
-          things at Wild.
+          <span className="whitespace-nowrap">
+            things at <span className="inline-block w-56" />
+            <WildLogoSVG className={cx("h-[0.8em] text-theme-color-text-primary", "absolute right-8 bottom-10")} />.
+          </span>
         </h1>
       </Text>
     </>
