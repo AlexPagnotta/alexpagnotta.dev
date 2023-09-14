c4780a77bfa1f4bcabc98279cee28a1412699e2b
Alex Pagnotta <ap.alex.pagnotta@gmail.com>
2023-09-14 22:11:42 +0200

    feat: add placeholder post card

--- app/content/posts/test-post/components/test-component.tsx
+++
@@ -1,7 +0,0 @@
-export const TestComponent = () => {
-  return (
-    <div>
-      <button className="px-12 py-24 bg-gray-20 text-black rounded-md">TestComponent</button>
-    </div>
-  );
-};
--- app/features/content/card/base.tsx
+++ app/features/content/card/base.tsx
@@ -25,13 +25,13 @@ export const BaseContentCard = ({ title, type, date, href, children, className }
   const formattedDate = date && formatContentCardDate(date);
 
   return (
-    <article className={cx("w-full h-full", className)}>
+    <article className={cx("w-full h-full pointer-events-none opacity-70", className)}>
       <Link href={href} className="w-full h-full">
         <div className={cx(cardStyles)}>
-          <Text size="title-2" asChild className="mb-16">
+          <Text size="title-2" asChild className="mb-12">
             <h2>{title}</h2>
           </Text>
-          <Text size="body-2" asChild className="mb-32 line-clamp-4 flex-1">
+          <Text size="body-2" asChild className="mb-16 line-clamp-4 flex-1">
             <p>{children}</p>
           </Text>
           <Text size="body-1" className="text-theme-color-text-secondary">
