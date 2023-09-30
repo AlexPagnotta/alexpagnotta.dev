11d3eed4d189a4fe2fcf592a67a01d96ddbb1cea
Alex Pagnotta <ap.alex.pagnotta@gmail.com>
2023-09-30 15:18:59 +0200

    feat: add homepage layout

--- app/(pages)/(homepage)/[[...category]]/page.tsx
+++ app/(pages)/(homepage)/[[...category]]/page.tsx
@@ -1,7 +1,6 @@
 import { CONTENT_CATEGORIES, categoryContentTypeMap, type ContentCategory } from "~/features/content/categories";
 import { ContentGrid } from "~/features/content/grid";
 import { getAllContentFrontMatters } from "~/features/content/utils.server";
-import { HomepageTitle } from "~/features/homepage/title";
 
 type Props = {
   params: {
@@ -22,14 +21,8 @@ const Home = async ({ params }: Props) => {
   const contentItems = await getAllContentFrontMatters(category ? categoryContentTypeMap[category] : undefined);
 
   return (
-    <div className="flex flex-col items-end gap-72 lg:gap-96">
-      <HomepageTitle className="text-theme-color-text-secondary">
-        Hello, I’m <span className="text-theme-color-text-primary">Alex Pagnotta</span>,{"\n"}a Frontend Developer from
-        Italy.{"\n"}Currently building things at <span className="text-theme-color-text-primary">Wild</span>.
-      </HomepageTitle>
-      <div className="w-full flex justify-center lg:justify-end">
-        <ContentGrid items={contentItems} />
-      </div>
+    <div className="w-full flex justify-center lg:justify-end">
+      <ContentGrid items={contentItems} />
     </div>
   );
 };
