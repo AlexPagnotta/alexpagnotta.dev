09d7e5bd558c0ae466626f3fc511f61807070b0e
Alex Pagnotta <ap.alex.pagnotta@gmail.com>
2024-01-15 22:46:02 +0100

    feat: add security headers

--- next.config.js
+++ next.config.js
@@ -41,6 +41,15 @@ const nextConfig = {
     return config;
   },
 
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
+
   async rewrites() {
     return {
       beforeFiles: [
@@ -60,5 +69,43 @@ const nextConfig = {
     };
   },
 };
+
+const ContentSecurityPolicy = `
+    default-src 'self' vercel.live;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live va.vercel-scripts.com;
+    style-src 'self' 'unsafe-inline' vercel.live;
+    img-src * blob: data:;
+    media-src 'self';
+    connect-src *;
+    font-src 'self' vercel.live;
+    frame-src 'self' *.alexpagnotta.dev ${process.env.NEXT_PUBLIC_LAB_3D_BASE_URL} vercel.live;
+`;
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\n/g, ""),
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains; preload",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()",
+  },
+];
 
 module.exports = withBundleAnalyzer(nextConfig);
